9e56fae61b045aa7e96976c6df82d18b40ac9276
s_watanabe <s_watanabe@K130301.kts-net.ktsystem.jp>
2014-10-16 14:35:01 +0900

    ＞FileNotFoundException のcatchは不要です。 FileNotFoundExceptionはIOExceptionのサブクラスなので、 AがなくなってもBでcatchされます。
    修正いたしました。
    勉強不足でした。ありがとうございます。

--- src/jp/ktsystem/kadai201408/s_watanabe/Kadai.java
+++ src/jp/ktsystem/kadai201408/s_watanabe/Kadai.java
@@ -4,7 +4,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -36,7 +35,6 @@ public class Kadai {
 	public static long calcScoreSum(String anInputPath) throws KadaiException {
 
 		BufferedReader br = null;
-		FileInputStream fis = null;
 		long calcScoreSum = 0;
 
 		if (null == anInputPath) {
@@ -60,27 +58,27 @@ public class Kadai {
 			if (null != str) {
 
 				// 要素を配列に詰める
-				String[] array = str.split(",");
+				String[] array = str.split(",", -1);
 
 				// 配列の要素を取り出して計算する
-				for (int n = 0; n < array.length; n++) {
+				for (int i = 0; i < array.length; i++) {
 
 					//
 					// 文字を数字に置き換える
 					//
 
 					// 半角英数の小文字を大文字に変換
-					String st = array[n].toUpperCase();
+					String st = array[i].toUpperCase();
 
 					// 数字・全角文字チェック
 					validateHalfWidthEnglish(st);
 
 					// 要素一つ一つに分解
 					int sum = 0;
-					for (int i = 0; i < st.length(); i++) {
+					for (int j = 0; j < st.length(); j++) {
 
 						// 一文字
-						char one = st.charAt(i);
+						char one = st.charAt(j);
 
 						// 要素の中身（一文字ずつバラバラにしたもの）を足す
 						long parts = ((int) one) - ((int) 'A' - 1);
@@ -89,7 +87,7 @@ public class Kadai {
 					}
 
 					// n + 1番目をかける
-					long multiplyValue = sum * (n + 1);
+					long multiplyValue = sum * (i + 1);
 
 					// 要素を一つずつ足す
 					calcScoreSum += multiplyValue;
@@ -97,21 +95,16 @@ public class Kadai {
 				}
 			}
 
-		} catch (FileNotFoundException e) {
-
-			throw new KadaiException(ErrorCode.FILE_IN_OUT);
-
 		} catch (IOException e) {
 
 			throw new KadaiException(ErrorCode.FILE_IN_OUT);
 
 		} finally {
 
-			if (null != br && null != fis) {
+			if (null != br) {
 
 				try {
 
-					fis.close();
 					br.close();
 
 				} catch (IOException e) {
